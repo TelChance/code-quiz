3e00448ec1318887ba0970c9c55f08365bff81a4
TelChance <92404288+TelChance@users.noreply.github.com>
2021-12-19 22:03:09 -0600

    Create java.js

---
+++ java.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question:"Commonly used data types DO Not include:",
+        a: "strings",
+        b: "booleans",
+        c: "alerts",
+        d: "numbers",
+        correct: "c",
+    },{
+        question:"The condition in an if/else statement is enclosed with ___.",
+        a: "quotes",
+        b: "curly brackets",
+        c: "parenthesis",
+        d: "square brackets",
+        correct: "c",
+    },{
+        question:"Arrays in JavaScript can be used to store",
+        a: "numbers and strings",
+        b: "other arrays",
+        c: "boleans",
+        d: "all of the above",
+        correct: "c",
+    },{
+        question:"String values must be enclosed within ____ when being assigned to variables.",
+        a: "commas",
+        b: "curly brackets",
+        c: "quotes",
+        d: "parenthesis",
+        correct: "d"
+    },{
+        question:"A very usefool tool used during development and debugging for printing content to the debugger is:",
+        a: "JavaScript",
+        b: "terminal/bash",
+        c: "for loops",
+        d: "console.log",
+        correct: "d",
+    }
+]
